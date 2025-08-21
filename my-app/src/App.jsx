79c4b8ab5950574bdebe7e4044d4d8d3255d73f2
Kohena Hazra <kohenahazra@gmail.com>
2025-08-21 12:44:34 +0530

    Revert "added medicalconsultationform.jsx and loginform.jsx"

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 // import Dashboard from "./components/Dashboard";
-// import Navbar from "./components/Navbar";
-// import Dashboard from "./components/Dashboard";
-// import Ngolist from "./components/Ngolist";
-import LoginForm from "./components/LoginForm";
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import Ngolist from "./components/Ngolist";
+
 
 function App() {
   return (
     <>
-    {/* <Navbar/> */}
-    {/* <Dashboard/> */}
-    <LoginForm/>
+    <Navbar/>
+    <Dashboard/>
     </>
   );
 }
