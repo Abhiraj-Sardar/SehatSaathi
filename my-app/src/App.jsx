2a74a0ea13e078e3f74fba7d343ff165d77a0630
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2025-08-21 17:36:40 +0530

    added routing

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
-import Ngolist from "./components/Ngolist";
 import LoginForm from "./components/LoginForm";
 import MedicalConsultationForm from "./components/MedicalConsultationForm";
 import NGOEventForm from "./components/NGOEventForm";
 import Home from "./components/Home";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    {/* <Navbar/>
-    <Dashboard/> */}
-    {/* <LoginForm/> */}
-    {/* <MedicalConsultationForm/> */}
-    {/* <NGOEventForm/> */}
-    <Home/>
+    {/*  */}
+    {/* <Home/> */}
+    
+    <BrowserRouter>
+        <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/Profile" element={<Dashboard/>}/>
+              <Route path="/Login" element={<LoginForm/>} />
+              <Route path="/ConsultationForm" element={<MedicalConsultationForm/>} />
+              <Route path="/ngoeventform" element={<NGOEventForm/>} />
+        </Routes>
+    </BrowserRouter>
     </>
   );
 }
