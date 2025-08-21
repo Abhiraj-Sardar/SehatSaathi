631445cbda62ee299632dd8d7214d4f500a1f93f
Kohena Hazra <kohenahazra@gmail.com>
2025-08-21 12:41:39 +0530

    added medicalconsultationform.jsxand loginform

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 // import Dashboard from "./components/Dashboard";
-import Navbar from "./components/Navbar";
-import Dashboard from "./components/Dashboard";
-import Ngolist from "./components/Ngolist";
-
+// import Navbar from "./components/Navbar";
+// import Dashboard from "./components/Dashboard";
+// import Ngolist from "./components/Ngolist";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Dashboard/>
+    {/* <Navbar/> */}
+    {/* <Dashboard/> */}
+    <LoginForm/>
     </>
   );
 }
